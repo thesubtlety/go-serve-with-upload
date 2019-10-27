af0ffaee158e16c8be3908020e018f6520c6b38f
thesubtlety <noah@thesubtlety.com>
2019-10-26 18:40:04 -0700

    add a few options

--- README.md
+++ README.md
@@ -6,9 +6,15 @@ Run `./gen-cert`
 
 ```
 Usage: ./server
+  -d string
+        directory to serve up (default ".")
   -h string
         interface to serve on (default "0.0.0.0")
   -k    don't use TLS
+  -kc string
+        path to cert file (default "server.pem")
+  -kf string
+        path to key file (default "server.key")
   -p string
         port to serve on (default "8000")
   -u string
--- server.go
+++ server.go
@@ -9,16 +9,20 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"path/filepath"
 )
 
 var user string
 var pass string
+var dir = flag.String("d", ".", "directory to serve up")
 
 func main() {
 	host := flag.String("h", "0.0.0.0", "interface to serve on")
 	port := flag.String("p", "8000", "port to serve on")
 	insecure := flag.Bool("k", false, "don't use TLS")
 	userpass := flag.String("u", "", "user:pass for basic auth")
+	certfile := flag.String("kc", "server.pem", "path to cert file")
+	keyfile := flag.String("kf", "server.key", "path to key file")
 	flag.Usage = usage
 	flag.Parse()
 
@@ -31,12 +35,12 @@ func main() {
 		user = strings.SplitN(*userpass, ":", 2)[0]
 		pass = strings.SplitN(*userpass, ":", 2)[1]
 		http.HandleFunc("/", requireAuth(mainHandler))
-		http.HandleFunc("/serve/", requireAuth(fsHandler("./", "/serve/")))
+		http.HandleFunc("/serve/", requireAuth(fsHandler(*dir, "/serve/")))
 		http.HandleFunc("/upload", requireAuth(uploadHandler))
 	} else {
 		log.Printf("Starting without authentication..")
 		http.HandleFunc("/", mainHandler)
-		http.HandleFunc("/serve/", fsHandler("./", "/serve/"))
+		http.HandleFunc("/serve/", fsHandler(*dir, "/serve/"))
 		http.HandleFunc("/upload", uploadHandler)
 	}
 
@@ -45,7 +49,7 @@ func main() {
 		log.Fatal(http.ListenAndServe(*host+":"+*port, nil))
 	} else {
 		log.Printf("Server started listening on https://%s:%s", *host, *port)
-		log.Fatal(http.ListenAndServeTLS(*host+":"+*port, "server.pem", "server.key", nil))
+		log.Fatal(http.ListenAndServeTLS(*host+":"+*port, *certfile, *keyfile, nil))
 	}
 }
 
@@ -69,8 +73,8 @@ func requireAuth(fn http.HandlerFunc) http.HandlerFunc {
 
 func checkCreds(u, p string) bool {
 	if u == user && p == pass {
-		return true
 		log.Printf("Successful auth for user %s", u)
+		return true
 	}
 	log.Printf("Got auth request for user %s", u)
 	return false
@@ -78,49 +82,50 @@ func checkCreds(u, p string) bool {
 
 func fsHandler(dir, prefix string) http.HandlerFunc {
 	fs := http.FileServer(http.Dir(dir))
-	h := http.StripPrefix(prefix, fs).ServeHTTP
+	serve := http.StripPrefix(prefix, fs).ServeHTTP
 	return func(w http.ResponseWriter, req *http.Request) {
 		logRequest(w, req)
-		log.Printf("Serving file %s", req.URL)
-		h(w, req)
+		serve(w, req)
 	}
 }
 
+//https://astaxie.gitbooks.io/build-web-application-with-golang/en/04.5.html
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Got upload request...")
 
 	if err := r.ParseMultipartForm(5 << (10 * 3)); // 5GB limit
 	err != nil {
+		log.Print(err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	file, handler, err := r.FormFile("file")
 	if err != nil {
+		log.Print(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	filename := handler.Filename
+	filename := filepath.Base(handler.Filename)
 	log.Printf("Uploading file %s", filename)
 	for k, v := range handler.Header {
-		log.Printf("%v:%v", k, v)
+		log.Printf("\t%v:%v", k, v)
 	}
 
-	f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	f, err := os.OpenFile(*dir + "/" + handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		fmt.Println(err)
+		log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer f.Close()
 
 	if _, err := io.Copy(f, file); err != nil {
+		log.Print(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Fatal(err)
 	}
 	log.Printf("File successfully uploaded...")
-	//fmt.Fprintf(w, "Successfully uploaded file!") //can't redirect if you display this
 
 	http.Redirect(w, r, "/serve", 302)
 }
@@ -131,14 +136,15 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.New("index").Parse(`
 <!DOCTYPE html>
 <head>
-        <title>Simple Go Serve...</title>
+        <title>Simple Transfer</title>
 <style>
-* {
-        font-family: "Helvetica", sans-serif;
-}
-.form{
-        border-radius:2px;
-        padding:5px;
+.content {
+	font-family: "Helvetica", sans-serif;
+    vertical-align: top;
+	margin: auto;
+	width: 600px;
+	padding: 20px;
+	border: 1px solid grey;
 }
 .button{
         background: #2472FE;
@@ -151,9 +157,9 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 </head>
 
 <body>
-<h1>Welcome</h1>
+<div class="content">
 <h3>Upload Files</h3>
-<form enctype="multipart/form-data" action="/upload" method="POST" class="form">
+<form enctype="multipart/form-data" action="/upload" method="POST">
                 <p label for="avatar">Choose a file to upload: </label><br/>
                 <input type="file" name="file" />
                 <input type="submit" value="Upload" class="button">
@@ -163,30 +169,28 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 <h3>Download Files</h3>
 <div><a href="/serve">Browse Files</a></div>
 
+</div>
 </body>
-</html>
-`)
+</html>`)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "It dun broke...", http.StatusInternalServerError)
+		log.Print(err)
 		return
 	}
 
 	t.Execute(w, r.RemoteAddr)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "It dun broke...", http.StatusInternalServerError)
+		log.Print(err)
 	}
 }
 
 func logRequest(w http.ResponseWriter, r *http.Request) {
-	log.Print("")
-	fmt.Printf("\t%s %s %s\n", r.Method, r.URL, r.Proto)
-	fmt.Printf("\tRemoteAddr = %q\n", r.RemoteAddr)
-	fmt.Printf("\tUser-Agent = %s\n", r.Header.Get("User-Agent"))
+	log.Printf("%s from %s, User-Agent = %s\n", r.URL, r.RemoteAddr, r.Header.Get("User-Agent"))
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
 	for k, v := range r.Form {
 		log.Printf("Form[%q] = %q\n", k, v)
 	}
-
 }
